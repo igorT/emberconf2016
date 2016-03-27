56087d9214aa43aed9fc2df7a8a12af11e117623
IgorT <terzicigor@gmail.com>
2016-03-27 05:23:59 -0700

    more progress

--- app/routes/dogs.js
+++ app/routes/dogs.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return [
+      {id:1,  name: 'dog1', image: "http://i3.kym-cdn.com/photos/images/newsfeed/000/217/040/48ACD.png", likes_cats: true, plays_fetch: false},
+      {id: 2, name: 'dog2', image: "http://www.ljplus.ru/img4/m/i/mister_trash/tumblr_lht7cvv39S1qamjl2o1_1280.jpg_AWSAccessKeyId_AKIAJ6IHWSU3BX3X7X3Q-Expires_1302934942-Signature_T1_2B9PPWHwEePu4hsLHTosJKnli4_3D", likes_cats: false, plays_fetch: true},
+      {id: 3, name: 'dog3', image: "http://i.imgur.com/sCcYF.jpg", likes_cats: true, plays_fetch: true},
+      {id: 4, name: 'dog4', image: "http://i.imgur.com/bRwvu16.jpg", likes_cats: true, plays_fetch: true}
+    ];
+  }
 });
--- tests/acceptance/cat-test.js
+++ tests/acceptance/cat-test.js
@@ -9,6 +9,7 @@ test('visiting /cat', function(assert) {
   andThen(function() {
     assert.equal(find('.hello').text(), ' Hello I am CAT', 'Cat says hi');
     assert.equal(find('.name').text(), 'cat1', 'Name of the first cat is cat1');
+    assert.equal(find('.owner').text(), 'Paul', 'Name of the owner of the first cat is Paul');
     
   });
 });
