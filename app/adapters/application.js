6045b8710a11b0040952cc29a320bbea2f7b9609
IgorT <terzicigor@gmail.com>
2016-03-28 14:52:46 -0700

    adapter works

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,58 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Adapter.extend({
+  namespace: 'api',
+  baseURLForType(type) {
+   let url = '';
+   if (this.namespace) {
+      url+= '/' + this.namespace;
+    }
+    url += '/' + this.keyForType(type);
+    url += '/';
+    return url;
+  },
+
+  keyForType(type) {
+    return Ember.String.pluralize(type.modelName);
+  },
+
+  findAll(store, type) {
+    return Ember.$.get(this.baseURLForType(type)).then((data) => {
+      let jsonArray = data[this.keyForType(type)];
+      let normalized = { data: jsonArray.map((cat) => ({ type: type.modelName, id: cat.id, attributes:cat })) };
+      return normalized;
+    });
+  },
+
+  findRecord(store, type, id) {
+    return Ember.$.get(this.baseURLForType(type) + id).then((data) => {
+      let catJSON = data[type.modelName];
+      return { data: { id: catJSON.id, type: type.modelName, attributes: catJSON } };
+    });
+  },
+
+  deleteRecord(store, type, snapshot) {
+    return Ember.$.ajax({
+      url:this.baseURLForType(type) + snapshot.id,
+      type: 'DELETE'
+    }).then((data) => {
+      return null;
+    });
+  },
+
+  updateRecord(store, type, snapshot) {
+    let toSend = {};
+    toSend[type.modelName] = snapshot.attributes();
+    toSend[type.modelName].id = snapshot.id;
+    return Ember.$.ajax({
+      url: this.baseURLForType(type) + snapshot.id,
+      type: 'PUT',
+      data: JSON.stringify(toSend)
+    }).then((data) => {
+      return null;
+    });
+  }
+
   /*
   findAll(store, type) {
 
--- app/adapters/cat.js
+++ app/adapters/cat.js
@@ -2,40 +2,4 @@ import ApplicationAdapter from './application';
 import Ember from 'ember';
 
 export default ApplicationAdapter.extend({
-  findAll(store, type) {
-    return Ember.$.get('/api/cats').then((data) => {
-      let jsonArray = data.cats;
-      let normalized = { data: jsonArray.map((cat) => ({ type: 'cat', id: cat.id, attributes:cat })) };
-      return normalized;
-    });
-  },
-
-  findRecord(store, type, id) {
-    return Ember.$.get('/api/cats/' + id).then((data) => {
-      let catJSON = data.cat;
-      return { data: { id: catJSON.id, type: 'cat', attributes: catJSON } };
-    });
-  },
-
-  deleteRecord(store, type, snapshot) {
-    return Ember.$.ajax({
-      url:'/api/cats/' + snapshot.id,
-      type: 'DELETE'
-    }).then((data) => {
-      return null;
-    });
-  },
-
-  updateRecord(store, type, snapshot) {
-    let toSend = { cat: snapshot.attributes() };
-    toSend['cat'].id = snapshot.id;
-    return Ember.$.ajax({
-      url:'/api/cats/' + snapshot.id,
-      type: 'PUT',
-      data: JSON.stringify(toSend)
-    }).then((data) => {
-      return null;
-    });
-  }
-
 });
