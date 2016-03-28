51c492d3c0ba9435975aacc22f4ee3db5d260b0a
IgorT <terzicigor@gmail.com>
2016-03-28 14:03:18 -0700

    jquery works

--- app/routes/cat.js
+++ app/routes/cat.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(/*params*/) {
-    return {"id":"1","name":"cat1","image":"http://24.media.tumblr.com/tumblr_m4j2e7Fd7M1qejbiro1_1280.jpg","owner_name":"Paul","relationships":{"cat_friends":[2,3],"best_friend":3}};
+  model: function(params) {
+    return Ember.$.get('/api/cats/' + params.id).then((data) => {
+      return data.cat;
+    });
+    //return {"id":"1","name":"cat1","image":"http://24.media.tumblr.com/tumblr_m4j2e7Fd7M1qejbiro1_1280.jpg","owner_name":"Paul","relationships":{"cat_friends":[2,3],"best_friend":3}};
   }
 });
