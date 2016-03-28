c1da2d2772d1c572a628c33dffa543ced22a5e51
IgorT <terzicigor@gmail.com>
2016-03-28 08:23:09 -0700

    Removing excess code

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,29 +2,4 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Adapter.extend({
-  findAll(store, type) {
-    return $.get('/api/' + Ember.String.pluralize(type.modelName));
-  },
-
-  findRecord(store, type, id, snapshot) {
-    return $.get('/api/' + Ember.String.pluralize(type.modelName) + '/' + id);
-  },
-
-  updateRecord(store, type, snapshot) {
-    let key =  type.modelName === 'cat' ? 'cat' : 'dog';
-    let toSend = {};
-    toSend[key] = snapshot.serialize();
-    return $.ajax({
-      method: 'PUT',
-      url: '/api/' + Ember.String.pluralize(type.modelName) + '/' + snapshot.id,
-      data: JSON.stringify(toSend)
-      }).then(function() { return null; });
-  },
-
-  deleteRecord(store, type, snapshot) {
-    return $.ajax({
-      method: 'DELETE',
-      url: '/api/' + Ember.String.pluralize(type.modelName) + '/' + snapshot.id,
-    }).then(function() { return null; });
-  }
 });
--- app/routes/cat.js
+++ app/routes/cat.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
-    //return {"id":"1","name":"cat1","image":"http://24.media.tumblr.com/tumblr_m4j2e7Fd7M1qejbiro1_1280.jpg","owner_name":"Paul","relationships":{"cat_friends":[2,3],"best_friend":3}};
-    return this.store.findRecord('cat', params.id);
-    //return $.get('api/cats/1').then((data) => { return data.cat; });
+  model: function(/*params*/) {
+    return {"id":"1","name":"cat1","image":"http://24.media.tumblr.com/tumblr_m4j2e7Fd7M1qejbiro1_1280.jpg","owner_name":"Paul","relationships":{"cat_friends":[2,3],"best_friend":3}};
   }
 });
