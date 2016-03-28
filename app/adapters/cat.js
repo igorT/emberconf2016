0b6ca8fdc37e7b842931b6cf186e705a25cc3f56
IgorT <terzicigor@gmail.com>
2016-03-28 10:33:48 -0700

    update and delete

--- app/adapters/cat.js
+++ app/adapters/cat.js
@@ -19,5 +19,22 @@ export default ApplicationAdapter.extend({
       let normalized = { data: { type: 'cat', id: payload.id, attributes: payload } };
       return normalized;
     });
-  }
+  },
+
+  deleteRecord(store, type, snapshot) {
+    return Ember.$.ajax('api/cats/' + snapshot.id, {
+      type: 'DELETE',
+    }).then((data) => null);
+  },
+
+  updateRecord(store, type, snapshot) {
+    let toSend = { cat: {
+      id: snapshot.id,
+      name: snapshot.attr('name')
+    }};
+    return Ember.$.ajax('api/cats/' + snapshot.id, {
+      type: 'PUT',
+      data: JSON.stringify(toSend)
+    }).then((data) => null);
+  },
 });
